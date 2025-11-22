dc330615607198b5d4a8c90a59ce0bec6f5c1df8
Yussuf Muzzammil <yussufmuzzamil247@gmail.com>
2025-11-22 12:12:50 -0800

    Completed CRUD Todo API assignment, made get(single read), mae the task field required and also created the get active

--- app.js
+++ app.js
@@ -12,13 +12,38 @@ app.get('/todos', (req, res) => {
   res.status(200).json(todos); // Send array as JSON
 });
 
-// POST New – Create
-app.post('/todos', (req, res) => {
-  const newTodo = { id: todos.length + 1, ...req.body }; // Auto-ID
-  todos.push(newTodo);
-  res.status(201).json(newTodo); // Echo back
+
+//GET SINGLE - Read
+app.get("/todos/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const todo = todos.find(t => t.id === id);
+
+  if (!todo) {
+    return res.status(404).json({ error: "Todo not found" });
+  }
+
+  res.json(todo);
 });
 
+
+// POST New – Create
+app.post("/todos", (req, res) => {
+  const { task } = req.body;
+
+  // VALIDATION
+  if (!task) {
+    return res.status(400).json({ error: "Task field is required" });
+  }
+
+  const newTodo = {
+    id: todos.length + 1,
+    task,
+    completed: false
+  };
+
+  todos.push(newTodo);
+  res.status(201).json(newTodo);
+});
 // PATCH Update – Partial
 app.patch('/todos/:id', (req, res) => {
   const todo = todos.find((t) => t.id === parseInt(req.params.id)); // Array.find()
@@ -41,10 +66,15 @@ app.get('/todos/completed', (req, res) => {
   const completed = todos.filter((t) => t.completed);
   res.json(completed); // Custom Read!
 });
+//GET ACTIVE TODOS - Read
+app.get("/todos/active", (req, res) => {
+  const activeTodos = todos.filter(t => t.completed === false);
+  res.json(activeTodos);
+});
 
 app.use((err, req, res, next) => {
   res.status(500).json({ error: 'Server error!' });
 });
 
-const PORT = 3002;
+const PORT = 3005;
 app.listen(PORT, () => console.log(`Server on port ${PORT}`));
